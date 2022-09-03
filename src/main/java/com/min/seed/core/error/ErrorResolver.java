754768aefc9b5910598c6b527c99cbc86f05efe3
minyangcheng <332485508@qq.com>
2022-09-03 14:21:23 +0800

    feat:修改全局异常处理

--- src/main/java/com/min/seed/core/error/ErrorResolver.java
+++ src/main/java/com/min/seed/core/error/ErrorResolver.java
@@ -9,9 +9,10 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.servlet.NoHandlerFoundException;
+import org.springframework.web.method.HandlerMethod;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 @ControllerAdvice
 @Slf4j
@@ -20,21 +21,19 @@ public class ErrorResolver {
     @ExceptionHandler(AuthException.class)
     @ResponseBody
     public Result authException(ServiceException e) {
-        log.error("authException", e);
         return ResultGenerator.genFailResult(e.getMessage());
     }
 
     @ExceptionHandler(ServiceException.class)
     @ResponseBody
     public Result serviceException(ServiceException e) {
-        log.error("serviceException", e);
         return ResultGenerator.genFailResult(e.getMessage());
     }
 
     @ExceptionHandler(Exception.class)
     @ResponseBody
-    public Result globalException(Exception e) {
-        log.error("globalException", e);
+    public Result globalException(HttpServletRequest request, HttpServletResponse response, Exception e) {
+        log.error("request uri [ " + request.getRequestURI()+" ] occur error", e);
         return ResultGenerator.genResult(ResultCode.INTERNAL_SERVER_ERROR, ResultCode.INTERNAL_SERVER_ERROR + ":" + e.getMessage());
     }
 
